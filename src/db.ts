6b3b3a7a84afb7294247cfd206273d3618fb0795
unknown <jacobdcutts@gmail.com>
2022-04-16 19:18:30 +0100

    working local with express routes

---
+++ src/db.ts
@@ -0,0 +1,12 @@
+import {Pool} from 'pg'
+import dotenv from 'dotenv'
+dotenv.config();
+
+export const pool = new Pool({
+    user: "postgres",
+    password: process.env.password,
+    host: "localhost",
+    port: 5432,
+    database: "portfolio"
+});
+
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,84 @@
-console.log("Nothin in index");
+import express from "express";
+import cors from "cors";
+import {pool} from "./db";
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const projectKeys = ["title", "language", "summary", "description", "image", "create_date", "difficulty"]
+
+//routes
+
+//create project
+app.post("/projects", async (req, res) => {
+    try {
+            if(Object.keys(req.body).every((key,i) => key === projectKeys[i])){ //check keys are correct and in order
+                const queryString = "INSERT INTO projects(title, language, summary, description, image, create_date, difficulty) VALUES($1,$2,$3,$4,$5,$6,$7) RETURNING *"
+                const newProject = await pool.query(queryString, Object.values(req.body));
+                res.json(newProject.rows[0])
+            }
+            else{
+                res.send("invalid project")
+            }
+    } catch (err) {
+        console.error(err)
+        res.send("invalid project")
+    }
+})
+
+//get all projects
+app.get("/projects", async (req, res) => {
+    try {
+        const allProjects = await pool.query("SELECT * FROM projects")
+        res.json(allProjects.rows)
+    } catch (err) {
+        console.error(err)
+    }
+})
+
+//get a project
+app.get("/projects/:project_id", async (req, res) => {
+    try{
+        const project = await pool.query("SELECT * FROM projects WHERE id=$1", [req.params.project_id])
+        res.json(project.rows)
+    } catch (err) {
+        console.error(err)
+    }
+})
+
+//update a project CANT RESOLVE COLUMN CANT BE A VAR
+app.put("/projects/:project_id", async (req, res) => {
+    try{
+        const [key, value] = Object.entries(req.body)[0];
+        if(projectKeys.includes(key)){
+            const projectCurrent = await pool.query("SELECT * FROM projects WHERE id=$1", [req.params.project_id])
+            projectCurrent.rows[0][key] = value; //update the specific key value
+            const queryString = "UPDATE projects SET title=$2, language=$3, summary=$4, description=$5, image=$6, create_date=$7, difficulty=$8 WHERE id=$1"
+            const projectUpdate = await pool.query(queryString, Object.values(projectCurrent.rows[0])); //don't update id column
+            res.json(projectUpdate.rows)
+        }else{
+            res.send("invalid key")
+        }
+    } catch (err) {
+        console.error(err)
+    }
+})
+
+//delete a project
+app.delete("/projects/:project_id", async (req, res) => {
+    try{
+        const project = await pool.query("DELETE FROM projects WHERE id=$1", [req.params.project_id])
+        res.json(project.rows)
+    } catch (err) {
+        console.error(err)
+    }
+})
+
+app.listen(5000, () => {
+    console.log("server is listening on port 5000")
+})
+
+
